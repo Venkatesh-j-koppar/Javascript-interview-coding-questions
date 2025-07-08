8507e19654732530cd6ef5eefb7f7c11bb625116
Venkatesh Koppar <41265841+Venkatesh-j-koppar@users.noreply.github.com>
2025-07-08 21:20:10 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -21,3 +21,24 @@ console.log(countValues(["apple", "banana", "apple", "orange", "banana"],2))
 output: 
 [ 'apple', 'banana' ]
 { apple: 2, banana: 2, orange: 1 }
+
+
+2. Find Second Largest Element
+
+const arr = [1,2,2,2,34,2,5,6,9,10]
+
+function findSecondLargest(arr){
+    let firstLargest = arr[0],secondLargest=arr[0]
+    for(let i=1;i<arr.length;i++){
+        if(arr[i]>firstLargest){
+          firstLargest = arr[i] 
+        }
+        if(arr[i]>secondLargest && arr[i]<firstLargest){
+            secondLargest = arr[i] 
+        }
+    }
+    return secondLargest
+}
+
+console.log(findSecondLargest(arr))
+output- 10 
