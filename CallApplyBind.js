6c68e9ce14f177e581922ce3de8693152fb81693
Venkatesh Koppar <41265841+Venkatesh-j-koppar@users.noreply.github.com>
2025-07-27 22:32:07 +0530

    Update CallApplyBind.js

--- CallApplyBind.js
+++ CallApplyBind.js
@@ -1,1 +1,33 @@
+// Call
+const obj1 = {
+  name:"Venkatesh"
+}
 
+function display(city,area){
+  console.log(this.name,city,area)
+}
+
+display.call(obj1,"Hubli","DeshpandeNagar")
+
+// Venkatesh Hubli DeshpandeNagar
+
+// Apply
+display.apply(obj1,["Hubli","DeshpandeNagar"])
+
+// Venkatesh Hubli DeshpandeNagar
+
+// Bind
+const displaying = display.bind(obj1,"Hubballi")
+
+displaying("Deshpande")
+
+// Venkatesh Hubballi DeshpandeNagar
+
+// Polyfill for Bind
+
+Function.prototype.myBind = function (thisArgs, ...params) {
+  const fn = this; // Save reference to the function that called myBind
+  return function (...args) {
+    return fn.apply(thisArgs, [...params, ...args]);
+  };
+};
