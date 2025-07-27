b79151579b05a57ecba670fbda041d735d2e9885
Venkatesh Koppar <41265841+Venkatesh-j-koppar@users.noreply.github.com>
2025-07-27 18:31:38 +0530

    Update Polyfiils.js

--- Polyfiils.js
+++ Polyfiils.js
@@ -1,1 +1,22 @@
+// Polyfills 
 
+// Map
+
+
+Array.prototype.myMap = function (callback,thisArgs) {
+    let result = []
+    for(let i=0;i<this.length;i++){
+        if(i in this){
+            result[i] = callback.call(thisArgs,this[i],i,this)
+        }   
+    }
+    return result
+}
+
+const obj1 = {
+    radius:5
+}
+
+// Note make sure u pass a normal function as a callback when u pass thisArgs as callback functions doesnot have this parameter in it.
+console.log(arr1.myMap((function(item){return item*this.radius}),obj1))
+console.log(arr1.myMap(((item) => item*2)))
