803ecc328aa8653d3bb1213058dd8d33600030a1
Venkatesh Koppar <41265841+Venkatesh-j-koppar@users.noreply.github.com>
2025-07-27 19:57:04 +0530

    Update Polyfiils.js

--- Polyfiils.js
+++ Polyfiils.js
@@ -20,4 +20,35 @@ console.log(arr1.myMap((function(item){return item*this.radius}),obj1)) // [ 5, 
 console.log(arr1.myMap(((item) => item*2))) // [ 2, 4, 6, 8, 10 ]
 
 
+// Filter
+
+const arr = [1,2,3,4,5,6,7,8,9]
+
+console.log(arr.filter(
+    function(item){
+        return item%2==0
+    }
+    ))
+// output [ 2, 4, 6, 8 ]
+    
+const obj1 = {
+    multiplier: 3
+}
+Array.prototype.myfilter = function (callback,thisArgs){
+    let result = []
+    for(let i=0;i<this.length;i++){
+        if(i in this){
+            if(callback.call(thisArgs,this[i],i,this)){
+                result.push(this[i])
+            }
+        }
+    }
+    return result
+}
+// Note make sure u pass a normal function as a callback when u pass thisArgs as callback functions doesnot have this parameter in it.
+console.log(arr.myfilter(function(item){return item%this.multiplier==0},obj1))
+
+// output [ 3, 6, 9 ]
+
+
 
